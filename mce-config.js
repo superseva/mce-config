03430efb8a22229e7122ee4f321a87f6eddc8002
superseva <superseva@gmail.com>
2020-08-28 16:14:42 +0200

    Remove Comments

--- mce-config.js
+++ mce-config.js
@@ -1,7 +1,4 @@
-Hooks.on("init", () => {
-            //console.log("I AM HERE AND READY");
-			CONFIG.TinyMCE.toolbar = "styleselect forecolor backcolor bullist numlist image table hr link removeformat code fontsizeselect save";
+Hooks.on("init", () => {            
+		CONFIG.TinyMCE.toolbar = "styleselect forecolor backcolor bullist numlist image table hr link removeformat code fontsizeselect save";
 });
-
-//Hooks.on("init", () => {CONFIG.debug.hooks = true});
 
